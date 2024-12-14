721f517ce638b2d49ef124d24ca364a7f6eab898
mncc8337 <mncc8327@gmail.com>
2024-12-14 12:49:38 +0700

    add environment

--- class/Building.gd
+++ class/Building.gd
@@ -2,17 +2,15 @@
 class_name Building
 extends Node3D
 
+func _ready() -> void:
+	self.scale = Vector3(1, 1, 1)
+
 func snap_to_grid() -> void:
 	self.position = self.position.snapped(Vector3i(1, 1, 1))
 	self.rotation = self.rotation.snapped(Vector3(PI/2, PI/2, PI/2))
 
-var old_position: Vector3
-var old_rotation: Vector3
 func _physics_process(_delta: float) -> void:
-	if old_position != self.position or old_rotation != self.rotation:
+	if Engine.is_editor_hint():
 		snap_to_grid()
+		self.scale = Vector3(1, 1, 1)
 	
-	old_position = self.position
-	old_rotation = self.rotation
-	
-	self.scale = Vector3(1, 1, 1)
