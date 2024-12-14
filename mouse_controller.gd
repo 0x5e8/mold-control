adc9489f95411560b1cd5046984eac2add1f73d7
mncc8337 <mncc8327@gmail.com>
2024-12-14 23:57:35 +0700

    even more refactor

--- mouse_controller.gd
+++ mouse_controller.gd
@@ -1,43 +1,26 @@
 extends Node
 
 var draggable = false
-
-@export var cam_weight = 0.2
 var target_pos: Vector3
 
 func _ready() -> void:
 	target_pos = %camera.global_position
 
 func _physics_process(delta: float) -> void:
-	%camera.global_position = lerp(%camera.global_position, target_pos, cam_weight)
+	%camera.global_position = lerp(%camera.global_position, target_pos, 0.4)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_MIDDLE:
 			draggable = event.pressed
 	
+	if event is InputEventMouseMotion and draggable:
+		var mouse_dir = event.relative.normalized() / 2.0
+		target_pos -= (%camera.global_basis.x * mouse_dir.x) - (%camera.basis.y * mouse_dir.y)
+	
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
-			MOUSE_BUTTON_LEFT: 
-				pass
-			MOUSE_BUTTON_RIGHT:
-				pass
-			MOUSE_BUTTON_MIDDLE:
-				draggable = !draggable
 			MOUSE_BUTTON_WHEEL_DOWN:
-				if %camera.fov < 75:
-					%camera.fov += 5
-				else:
-					%camera.fov = 75
+				target_pos += %camera.global_basis.z * 2
 			MOUSE_BUTTON_WHEEL_UP:
-				if %camera.fov > 5:
-					%camera.fov -= 5
-				else:
-					%camera.fov = 5
-	
-	if event is InputEventMouseMotion and draggable:
-		target_pos -= mouse_movement(event)
-	
-func mouse_movement(event):
-	var mouse_dir = event.relative.normalized() / 3.0
-	return (%camera.global_basis.x * mouse_dir.x) - (%camera.basis.y * mouse_dir.y)
+				target_pos -= %camera.global_basis.z * 2
