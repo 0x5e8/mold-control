aebc79cf2cb838f3d6ead9ed9c18474d641ceb8d
mncc8337 <mncc8327@gmail.com>
2024-12-14 23:46:24 +0700

    idk what have i done but it fixed now

--- mouse_controller.gd
+++ mouse_controller.gd
@@ -18,15 +18,12 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
-<<<<<<< HEAD
-=======
 			MOUSE_BUTTON_LEFT: 
 				pass
 			MOUSE_BUTTON_RIGHT:
 				pass
 			MOUSE_BUTTON_MIDDLE:
 				draggable = !draggable
->>>>>>> 273c5267659b17bf2eaa29b8af8dfab1ad46ff5f
 			MOUSE_BUTTON_WHEEL_DOWN:
 				if %camera.fov < 75:
 					%camera.fov += 5
@@ -37,7 +34,6 @@ func _unhandled_input(event: InputEvent) -> void:
 					%camera.fov -= 5
 				else:
 					%camera.fov = 5
-<<<<<<< HEAD
 	
 	if event is InputEventMouseMotion and draggable:
 		target_pos -= mouse_movement(event)
@@ -45,13 +41,3 @@ func _unhandled_input(event: InputEvent) -> void:
 func mouse_movement(event):
 	var mouse_dir = event.relative.normalized() / 3.0
 	return (%camera.global_basis.x * mouse_dir.x) - (%camera.basis.y * mouse_dir.y)
-=======
-
-	if event is InputEventMouseMotion and draggable:
-			target -= mouse_movement(event)
-			
-func mouse_movement(event):
-	
-	var mouse_dir = event.relative.normalized()
-	return clamp(speed,0.1,c_speed)*((%camera.global_basis.x * mouse_dir.x) - (%camera.basis.y * mouse_dir.y))
->>>>>>> 273c5267659b17bf2eaa29b8af8dfab1ad46ff5f
